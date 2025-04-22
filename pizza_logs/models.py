3abbadbad881808a649e74ed29b66defa50db0a0
Alexey Popkov <alvlpv969@gmail.com>
2025-04-22 16:47:29 +1000

    Komsomol-45-pizza

---
+++ pizza_logs/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Pizza(models.Model):
+    """Тема, которую изучает пользователь."""
+    name = models.CharField(max_length=30)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Пицца'
+        verbose_name_plural = 'Пицца'
+
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        return self.name
+
+
+class Topping(models.Model):
+    """Информация, изученная пользователем по теме."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'Начинка'
+        verbose_name_plural = 'Начинка'
+        
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        return f"{self.name}"
